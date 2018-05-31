dbdef6ec37e088020fb4c7ed68f485a9f7117b30
Siphiwe Mkhize <siphiwemkhize@yahoo.com>
2018-05-31 18:14:27 +0200

    Add project files.

---
+++ CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using CMSShoppingCart.Models.Data;
+using CMSShoppingCart.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMSShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            List<PageVM> pagesList;
+
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+                return View(pagesList);
+        }
+    }
+}
